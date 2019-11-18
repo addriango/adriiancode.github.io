49d950f34d09d82ee1009d3ba1b9c6a448842e6b
Adrian Gomez <adriandgomez123@gmail.com>
2019-11-18 14:12:52 -0500

    menu hamburguesa para moviles

--- js/lo.js
+++ js/lo.js
@@ -1,1 +1,12 @@
-console.log("hola");
+const nav = document.querySelector(".navigation");
+const boton = document.querySelector("#burger-menu");
+
+boton.addEventListener("click", show_nav);
+
+function show_nav() {
+  if (nav.classList.contains("is-active")) {
+    nav.classList.remove("is-active");
+  } else {
+    nav.classList.add("is-active");
+  }
+}
